b0f90e729e9f1c22feb8a6f15980d95d2864f4f6
your username <ebukareed@gmail.com>
2022-03-16 11:04:35 +0100

    updates

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - print alphabet
+*
+* Returb: void
+*/
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
